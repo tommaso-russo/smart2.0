511c9b5cf689f3586e79d18195199bf0edd091b9
Tommaso Russo <Tommaso.Russo@Uniroma2.it>
2025-03-03 18:36:48 +0100

    Add files via upload
    Function to add Landing Weight and Landing Value to a IBM object

---
+++ add_WLV.R
@@ -0,0 +1,11 @@
+add_WLV = function(x, tpar){
+  x = x %>% dplyr::select(-any_of(c("W", "GVL")))
+  x = left_join(x, tpar)
+  x$W = x$effort * x$lpue 
+  x$GVL = x$tGVL * x$W * x$Price
+  x = x %>% dplyr::select(-any_of(c("tEC", "tOC", "tLC", "tGVL")))
+  return(x)
+}
+
+
+
