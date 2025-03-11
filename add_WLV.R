dbf2fba3660fdd33ffc1773c2202a438350719e9
Tommaso Russo <Tommaso.Russo@Uniroma2.it>
2025-03-11 09:08:30 +0100

    Add files via upload

--- add_WLV.R
+++ add_WLV.R
@@ -1,6 +1,6 @@
 add_WLV = function(x, tpar){
   x = x %>% dplyr::select(-any_of(c("W", "GVL")))
-  x = left_join(x, tpar)
+  x = suppressMessages(left_join(x, tpar))
   x$W = x$effort * x$lpue 
   x$GVL = x$tGVL * x$W * x$Price
   x = x %>% dplyr::select(-any_of(c("tEC", "tOC", "tLC", "tGVL")))
