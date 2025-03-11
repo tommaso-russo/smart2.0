6b37c7ce97002683c5199bb5cd193b6c7bf226d9
Tommaso Russo <Tommaso.Russo@Uniroma2.it>
2025-03-11 19:45:44 +0100

    Update IBM2eco.R

--- IBM2eco.R
+++ IBM2eco.R
@@ -4,14 +4,18 @@ IBM2eco = function(x, params, df_harbs_grid_dists,
   
   setDT(x)
   
-  x_agg_fishing_time <- unique(x, by = c("CFR", "YEAR", "MONTH", "Gear", 
-                                         "Metier", "harbour", "id_grid", "fishing_time"))  
+  x_agg_fishing_time <-  x %>% dplyr::select(c("CFR", "YEAR", "MONTH", "Gear", "Metier", 
+                                               "harbour", "loa", "ntrip", "fishing_time", "id_grid")) %>% distinct()
+    
+  
   x_agg_fishing_time =  aggregate(data = x_agg_fishing_time,
                                  fishing_time ~ ., FUN = "sum" , na.rm = TRUE)
   
-  x_agg_fishing_time =  left_join(x_agg_fishing_time, 
-                                                   df_harbs_grid_dists, 
-                                                   relationship = "many-to-many")
+  x_agg_fishing_time =  left_join(x_agg_fishing_time,
+                                  df_harbs_grid_dists, 
+                                  by = c("id_grid", "harbour"))
+  
+  x_agg_fishing_time =  x_agg_fishing_time[complete.cases(x_agg_fishing_time), ]
   
   x_agg_WD = x_agg_fishing_time %>%
     group_by(CFR, YEAR, MONTH, Gear, Metier, harbour) %>%
@@ -27,20 +31,20 @@ IBM2eco = function(x, params, df_harbs_grid_dists,
 
   # Combine alltogheter
   x_agg = inner_join(as.data.frame(x_agg_sum), 
-                                      as.data.frame(x_agg_WD), 
-                                      relationship = "many-to-many")
+                                   as.data.frame(x_agg_WD), 
+                                   relationship = "many-to-many")
   x_agg = inner_join(x_agg, x_agg_mean, 
                                       relationship = "many-to-many")
   x_agg = inner_join(x_agg, 
                      aggregate(data = x_agg_fishing_time,
                                       fishing_time ~ CFR + YEAR + MONTH + Gear + Metier + harbour,
-                                      FUN = "sum"), 
-                                      relationship = "many-to-many")
+                                      FUN = "sum"))
+  
   x_agg = left_join(x_agg, tpar, relationship = "many-to-many")
   x_agg$steaming_time = tfuel * (x_agg$MeanWeightDistance/steaming_speed) * x_agg$ntrip
   x_agg = left_join(x_agg, params, relationship = "many-to-many")
   x_agg$FC = x_agg$q * x_agg$loa^x_agg$m * (x_agg$fishing_time + x_agg$steaming_time)
-  x_agg = left_join(x_agg, fuel_cost, relationship = "many-to-many")
+  x_agg = left_join(x_agg, fuel_cost)
   x_agg$EC = x_agg$tEC * (x_agg$FC * x_agg$fuel_cost)
   x_agg$OC = x_agg$tOC * x_agg$GVL * OCfact
   x_agg$INV = x_agg$GVL * INVfact
@@ -50,7 +54,3 @@ IBM2eco = function(x, params, df_harbs_grid_dists,
   x_agg = x_agg %>% dplyr::select(-c(tEC, tOC, tLC))
   return(x_agg)
 }
-
-
-
-
