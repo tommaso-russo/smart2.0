2f0da31459ffd4326d631ff739aee7206e2e1acb
Tommaso Russo <Tommaso.Russo@Uniroma2.it>
2025-03-12 18:01:56 +0100

    Update update.IBM.sim.R

--- update.IBM.sim.R
+++ update.IBM.sim.R
@@ -1,8 +1,8 @@
 update.IBM.sim <- function(IBM.sim.ref, IBM.sim, improved_patterns, key_columns){
   if(nrow(improved_patterns) > 0){
     cases_to_update <- improved_patterns %>% select(all_of(key_columns)) %>% distinct()
-    IBM.sim.ref <- IBM.sim.ref %>% anti_join(cases_to_update, by = key_columns)
-    IBM_2add <- IBM.sim %>% semi_join(cases_to_update, by = key_columns)
+    IBM.sim.ref <- anti_join(IBM.sim.ref, cases_to_update, by = key_columns)
+    IBM_2add <- semi_join(IBM.sim, cases_to_update, by = key_columns)
     IBM.sim.ref <- bind_rows(IBM.sim.ref, IBM_2add)
   }
   return(IBM.sim.ref)
