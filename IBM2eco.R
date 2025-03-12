0e5a3a9b0e50fb29dc8bf1a23973d12e3e2d245e
Tommaso Russo <Tommaso.Russo@Uniroma2.it>
2025-03-12 22:52:40 +0100

    Update IBM2eco.R

--- IBM2eco.R
+++ IBM2eco.R
@@ -2,14 +2,12 @@ IBM2eco = function(x, params, df_harbs_grid_dists,
                    fuel_cost, OCfact, INVfact, LCfact, 
                    tpar){
   
-  setDT(x)
   
   x_agg_fishing_time <-  x %>% dplyr::select(c("CFR", "YEAR", "MONTH", "Gear", "Metier", 
                                                "harbour", "loa", "ntrip", "fishing_time", "id_grid")) %>% distinct()
-    
   
   x_agg_fishing_time =  aggregate(data = x_agg_fishing_time,
-                                 fishing_time ~ ., FUN = "sum" , na.rm = TRUE)
+                                  fishing_time ~ ., FUN = "sum" , na.rm = TRUE)
   
   x_agg_fishing_time =  left_join(x_agg_fishing_time,
                                   df_harbs_grid_dists, 
@@ -17,28 +15,40 @@ IBM2eco = function(x, params, df_harbs_grid_dists,
   
   x_agg_fishing_time =  x_agg_fishing_time[complete.cases(x_agg_fishing_time), ]
   
+  
+  # dim(x_agg_fishing_time %>% dplyr::select(CFR, MONTH, Gear, Metier, loa, ntrip, harbour) %>% distinct())
+  
+  
   x_agg_WD = x_agg_fishing_time %>%
-    group_by(CFR, YEAR, MONTH, Gear, Metier, harbour) %>%
+    group_by(CFR, YEAR, MONTH, Gear, Metier, harbour, loa, ntrip) %>%
     summarise(MeanWeightDistance = weighted.mean(distance, fishing_time, na.rm = TRUE), .groups = "drop")
+ 
+  # dim(x_agg_WD %>% dplyr::select(CFR, MONTH, Gear, Metier, loa, ntrip, harbour) %>% distinct())
   
   x_agg_sum = x %>%
-    group_by(CFR, YEAR, MONTH, Gear, Metier, harbour) %>%
+    group_by(CFR, MONTH, Gear, Metier, harbour, loa, ntrip) %>%
     summarize(across(c(W, GVL), sum, na.rm = TRUE))
-      
-  x_agg_mean = x %>%
-    group_by(CFR, YEAR, MONTH, Gear, Metier, harbour) %>%
-    summarize(across(c(loa, ntrip), mean, na.rm = TRUE))
-
+  
+  # dim(x_agg_sum %>% dplyr::select(CFR, MONTH, Gear, Metier, loa, ntrip, harbour) %>% distinct())
+  
+  #x_agg_mean = x %>%
+  # group_by(CFR, MONTH, Gear, Metier, harbour, loa, ntrip) %>% distinct()
+   
+  # dim(x_agg_mean %>% dplyr::select(CFR, MONTH, Gear, Metier, loa, ntrip, harbour) %>% distinct())
+  
+  
   # Combine alltogheter
   x_agg = inner_join(as.data.frame(x_agg_sum), 
-                                   as.data.frame(x_agg_WD), 
-                                   relationship = "many-to-many")
-  x_agg = inner_join(x_agg, x_agg_mean, 
-                                      relationship = "many-to-many")
+                     as.data.frame(x_agg_WD))
+  # dim(x_agg %>% dplyr::select(CFR, MONTH, Gear, Metier, loa, ntrip, harbour) %>% distinct())
+  
+  
+  # x_agg = inner_join(x_agg, x_agg_mean, 
+  #                    relationship = "many-to-many")
   x_agg = inner_join(x_agg, 
                      aggregate(data = x_agg_fishing_time,
-                                      fishing_time ~ CFR + YEAR + MONTH + Gear + Metier + harbour,
-                                      FUN = "sum"))
+                               fishing_time ~ CFR + YEAR + MONTH + Gear + Metier + harbour,
+                               FUN = "sum"))
   
   x_agg = left_join(x_agg, tpar, relationship = "many-to-many")
   x_agg$steaming_time = tfuel * (x_agg$MeanWeightDistance/steaming_speed) * x_agg$ntrip
