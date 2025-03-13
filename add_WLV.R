cdd8f7f8a9d24cb46af42021343a990f50b25f77
Tommaso Russo <Tommaso.Russo@Uniroma2.it>
2025-03-13 20:29:07 +0100

    Update add_WLV.R

--- add_WLV.R
+++ add_WLV.R
@@ -1,12 +1,8 @@
 add_WLV = function(x, tpar){
   x = x %>% dplyr::select(-any_of(c("W", "GVL")))
-  x = left_join(x, tpar)
+  x = suppressMessages(left_join(x, tpar))
   x$W = 10 * x$effort * x$lpue
   x$GVL = x$tGVL * x$W * x$Price
   x = x %>% dplyr::select(-any_of(c("tEC", "tOC", "tLC", "tGVL")))
   return(x)
 }
-
-
-
-
